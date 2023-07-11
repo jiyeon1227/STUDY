570b0b430459b9a429373334059f04dbe246228c
jiyeon1227 <74654224+jiyeon1227@users.noreply.github.com>
2023-07-11 17:18:26 +0900

    Add files via upload

---
+++ javaproject/src/p2023_07_11/Test.java
@@ -0,0 +1,11 @@
+package p2023_07_11;
+
+public class Test {
+
+	public static void main(String[] args) {
+		//syso 입력하고 컨트롤 + 스페이스바 -> 출력문 자동완성 단축키
+		//윈도우탭 -> perspective -> recent perspective 하면 초기상태로 돌아감
+		System.out.println("오늘은 7월 11일");
+	}
+
+}
