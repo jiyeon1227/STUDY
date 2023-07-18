f24f571073f8cbc5c87cbd6b621a9677b95917e3
jiyeon1227 <74654224+jiyeon1227@users.noreply.github.com>
2023-07-18 17:51:25 +0900

    Add files via upload

---
+++ p2023_07_17/Report_1.java
@@ -0,0 +1,27 @@
+package p2023_07_17;
+import java.util.Scanner;
+
+public class Report_1 {//Factorial
+	
+	//2023_07_17 과제1 과제풀이
+
+	// 재귀함수 : 함수안에서 자기자신의 함수를 호출하는 함수
+	static int fact(int n) {
+		if(n<=1) {
+			return 1;
+		}
+		return n * fact(n-1);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("원하는 팩토리얼을 입력하세요?");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int result = fact(n);
+		System.out.println(n+"!="+ result);
+		
+	}
+
+}
