d7782389a797d465ecf38584237cbd9e0c58fbe8
jiyeon1227 <74654224+jiyeon1227@users.noreply.github.com>
2023-07-21 17:03:41 +0900

    Add files via upload

---
+++ a/b/Called.java
@@ -0,0 +1,11 @@
+package a.b;
+
+//접근제어자 실습
+
+public class Called {
+
+	public void check() {
+		System.out.println("메소드 호출 성공 ~");
+	}
+	
+}
---
+++ c/Calling.java
@@ -0,0 +1,15 @@
+package c;
+import a.b.Called;
+
+//접근제어자 실습
+
+public class Calling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Called c = new Called();
+		c.check();
+	}
+
+}
