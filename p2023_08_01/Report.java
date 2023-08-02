0db87691db1ae4c28fa5d512a5e373f9a35d736e
jiyeon1227 <74654224+jiyeon1227@users.noreply.github.com>
2023-08-02 10:09:39 +0900

    Add files via upload

---
+++ p2023_08_01/Report.java
@@ -0,0 +1,28 @@
+﻿package p2023_08_01;
+
+//2023_08_02 과제 풀이
+
+import java.util.*;
+
+public class Report {// SetTest01
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n;
+//		Set s = new HashSet();// 오름차순 정렬은 안됨
+		TreeSet s = new TreeSet();
+
+		Random r = new Random();
+
+		while (true) {
+			n = r.nextInt(45) + 1; // 1 ~ 45 난수 발생
+			s.add(n);
+			if (s.size() == 6) {
+				System.out.println(s);
+				break;
+			}
+		}
+	}
+
+}
