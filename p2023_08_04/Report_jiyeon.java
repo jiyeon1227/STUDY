c37eeaf58274d4e31590106ba0c735dbf12ba434
jiyeon1227 <74654224+jiyeon1227@users.noreply.github.com>
2023-08-08 19:10:07 +0900

    Add files via upload

--- p2023_08_04/Report_jiyeon.java
+++ p2023_08_04/Report_jiyeon.java
@@ -1,7 +1,8 @@
 package p2023_08_04;
 
+import java.io.BufferedReader;
 import java.io.FileWriter;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 //과제1. 키보드로 입력한 문장을 파일(result.txt)로 저장하는 프로그램
 
@@ -10,6 +11,21 @@ public class Report_jiyeon {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			System.out.println("문장을 입력하세요");
+			String str = br.readLine();
+			
+			FileWriter fw = new FileWriter("result.txt");
+			fw.write(str);
+			fw.close();
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		
+		/* Scanner로 입력받을 때
 		System.out.println("문장을 입력하세요");
 		Scanner sc = new Scanner(System.in);
 		
@@ -22,6 +38,7 @@ public class Report_jiyeon {
 		}catch(Exception e){
 			System.out.println(e);
 		}
+		*/
 		
 	}
 
