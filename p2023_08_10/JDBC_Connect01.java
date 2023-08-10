c9cc98ff9f5d423a630321aa00f16845be592605
jiyeon1227 <74654224+jiyeon1227@users.noreply.github.com>
2023-08-10 17:02:17 +0900

    Add files via upload

---
+++ p2023_08_10/JDBC_Connect01.java
@@ -0,0 +1,27 @@
+package p2023_08_10;
+
+import java.sql.*;
+
+public class JDBC_Connect01{
+
+  public static void main(String[] args)  {
+
+/** ORACLE JDBC Driver Test ***************************/
+//	String driver = "oracle.jdbc.driver.OracleDriver";  
+/******************************************************/
+
+/** My-SQL JDBC Driver Test **************************/	
+//	String driver ="com.mysql.jdbc.Driver";// 5.x
+	String driver ="com.mysql.cj.jdbc.Driver";//8.x
+/*****************************************************/
+
+    try{
+      Class.forName(driver);
+      System.out.println("JDBC Driver Loading 성공~!!");
+
+    }catch(Exception e){
+      System.out.println("JDBC Driver Loading 실패~!!");
+	  e.printStackTrace();
+    }
+ }
+}  
